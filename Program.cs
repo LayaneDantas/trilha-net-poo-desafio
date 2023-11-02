01059614da254387878dc102bf49809ec2552d48
Layane Dantas Formiga <layanedantasformiga@gmail.com>
2023-11-02 15:58:52 -0300

    Desafio Realizado com Sucesso!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "1234", modelo: "14plus", imei:"11111111", memoria:64 );
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Jogo do Tigre");
+
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "5678", modelo: "MotoG10Play", imei: "22222222", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Pou");
+    
